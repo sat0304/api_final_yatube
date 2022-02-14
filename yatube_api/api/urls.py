e740c36babfe1cfd63c9baffaf34b22f1bb9190e
Сергей Токарев <sat0304@yandex.ru>
2022-02-14 12:58:19 +0400

    urls and settigs were changed

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,25 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .views import CommentViewSet, GroupViewSet, PostViewSet
 
+
+app_name = 'api'
+
+router_v1 = DefaultRouter()
+router_v1.register('posts', PostViewSet, basename='posts')
+router_v1.register('groups', GroupViewSet, basename='groups')
+router_v1.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
+router_v1.register(
+    r'posts/(?P<post_id>\d+)/comments/(?P<comment_id>\d+)',
+    CommentViewSet,
+    basename='comment'
+)
 urlpatterns = [
+    path('v1/', include(router_v1.urls)),
+    # path('v1/auth/', include('djoser.urls')),
+    path('v1/auth/', include('djoser.urls.jwt')),
 ]
